58e8a9f2544de36d08813f2d35d598c03003d446
InochiTanaka <yunominosencha@gmail.com>
2018-04-25 19:08:43 -0700

    Initial commit

---
+++ ColorSizeMe/ColorSizeMe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ColorSizeMe.Startup))]
+namespace ColorSizeMe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
